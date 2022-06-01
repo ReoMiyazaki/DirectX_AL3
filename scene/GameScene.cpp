9642babf5e86468b19598f8d55eff8cd73e1dfd5
ReoMiyazaki <k021g1153@g.neec.ac.jp>
2022-06-01 11:06:10 +0900

    afin3D_02

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,8 +1,8 @@
 ﻿#include "GameScene.h"
-#include "TextureManager.h"
-#include <cassert>
 #include "AxisIndicator.h"
 #include "PrimitiveDrawer.h"
+#include "TextureManager.h"
+#include <cassert>
 
 GameScene::GameScene() {}
 
@@ -39,11 +39,71 @@ void GameScene::Initialize() {
 	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
 	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
 
-	
+	for (int i = 0; i < 8; i++) {
+
+		//平行移動
+		vertexTranslation[i].x = vertex[i].x * Translation[0][0] + vertex[i].y * Translation[0][1] +
+		                         vertex[i].z * Translation[0][2] +
+		                         1 * Translation[0][3]; // X軸方向の移動
+
+		vertexTranslation[i].y = vertex[i].x * Translation[1][0] + vertex[i].y * Translation[1][1] +
+		                         vertex[i].z * Translation[1][2] +
+		                         1 * Translation[1][3]; // Y軸方向の移動
+
+		vertexTranslation[i].z = vertex[i].x * Translation[2][0] + vertex[i].y * Translation[2][1] +
+		                         vertex[i].z * Translation[2][2] +
+		                         1 * Translation[2][3]; // Z軸方向の移動
+	}
 
 
+	for (int i = 0; i < 8; i++) {
+		// X軸周りの回転
+		vertexRotateX[i].x =
+		  vertex[i].x * RotateX[0][0] + vertex[i].y * RotateX[0][1] + vertex[i].z * RotateX[0][2];
 
-	
+		vertexRotateX[i].y =
+		  vertex[i].x * RotateX[1][0] + vertex[i].y * RotateX[1][1] + vertex[i].z * RotateX[1][2];
+
+		vertexRotateX[i].z =
+		  vertex[i].x * RotateX[2][0] + vertex[i].y * RotateX[2][1] + vertex[i].z * RotateX[2][2];
+	}
+
+	for (int i = 0; i < 8; i++) {
+		// Y軸周りの回転
+		vertexRotateY[i].x =
+		  vertex[i].x * RotateY[0][0] + vertex[i].y * RotateY[0][1] + vertex[i].z * RotateY[0][2];
+
+		vertexRotateY[i].y =
+		  vertex[i].x * RotateY[1][0] + vertex[i].y * RotateY[1][1] + vertex[i].z * RotateY[1][2];
+
+		vertexRotateY[i].z =
+		  vertex[i].x * RotateY[2][0] + vertex[i].y * RotateY[2][1] + vertex[i].z * RotateY[2][2];
+
+	}
+
+	for (int i = 0; i < 8; i++) {
+		// Z軸周りの回転
+		vertexRotateZ[i].x =
+		  vertex[i].x * RotateZ[0][0] + vertex[i].y * RotateZ[0][1] + vertex[i].z * RotateZ[0][2];
+
+		vertexRotateZ[i].y =
+		  vertex[i].x * RotateZ[1][0] + vertex[i].y * RotateZ[1][1] + vertex[i].z * RotateZ[1][2];
+
+		vertexRotateZ[i].z =
+		  vertex[i].x * RotateZ[2][0] + vertex[i].y * RotateZ[2][1] + vertex[i].z * RotateZ[2][2];
+	}
+
+	for (int i = 0; i < 8; i++) {
+		// 拡縮
+		vertexScale[i].x =
+		  vertex[i].x * Scale[0][0] + vertex[i].y * Scale[0][1] + vertex[i].z * Scale[0][2];
+
+		vertexScale[i].y =
+		  vertex[i].x * Scale[1][0] + vertex[i].y * Scale[1][1] + vertex[i].z * Scale[1][2];
+
+		vertexScale[i].z =
+		  vertex[i].x * Scale[2][0] + vertex[i].y * Scale[2][1] + vertex[i].z * Scale[2][2];
+	}
 }
 
 void GameScene::Update() { debugCamera_->Update(); }
@@ -75,12 +135,27 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
-	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
-	//model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	// model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	// model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
 	for (int i = 0; i < 12; i++) {
-		PrimitiveDrawer::GetInstance()->DrawLine3d(vertex[edgeList[i][0]], vertex[edgeList[i][1]],color);
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		  vertex[edgeList[i][0]], vertex[edgeList[i][1]], WHITE);
+
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		  vertexTranslation[edgeList[i][0]], vertexTranslation[edgeList[i][1]], RED);
+
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		  vertexRotateX[edgeList[i][0]], vertexRotateX[edgeList[i][1]], GREEN);
+
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		  vertexRotateY[edgeList[i][0]], vertexRotateY[edgeList[i][1]], GREEN);
+
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		  vertexRotateZ[edgeList[i][0]], vertexRotateZ[edgeList[i][1]], GREEN);
+
+		PrimitiveDrawer::GetInstance()->DrawLine3d(
+		  vertexScale[edgeList[i][0]], vertexScale[edgeList[i][1]], BLACK);
 	}
-	
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
